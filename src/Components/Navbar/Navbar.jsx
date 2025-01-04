51ee87ada4cbf38eaf977815a7dccff71c47e443
thakur7071 <singhnaveen2370@gmail.com>
2025-01-04 16:49:54 +0530

    Initial Commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,53 +1,100 @@
-import './Navbar.css'
-import cart_icon from '../Assets/cart_icon.png'
-import mainlogo from '../Assets/mainlogo.jpg'
-import { useRef, useState } from 'react'
-import { Link, } from 'react-router-dom'
-import loginicon from '../Assets/loginicon.png'
-import navdropdown from '../Assets/navdropdown-icon.png'
-const Navbar = ({setShowLogin}) => {
+import "./Navbar.css";
+import cart_icon from "../Assets/cart_icon.png";
+import mainlogo from "../Assets/mainlogo.jpg";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import loginicon from "../Assets/loginicon.png";
+import navdropdown from "../Assets/navdropdown-icon.png";
+const Navbar = ({ setShowLogin }) => {
+  document.querySelectorAll(".nav-menu li").forEach((item) => {
+    item.addEventListener("click", () => {
+      // Remove active class from all list items
+      document
+        .querySelectorAll(".nav-menu li")
+        .forEach((el) => el.classList.remove("active"));
 
-  document.querySelectorAll('.nav-menu li').forEach(item => {
-    item.addEventListener('click', () => {
-        // Remove active class from all list items
-        document.querySelectorAll('.nav-menu li').forEach(el => el.classList.remove('active'));
-        
-        // Add active class to the clicked item
-        item.classList.add('active');
+      // Add active class to the clicked item
+      item.classList.add("active");
     });
-});
+  });
 
+  const [menu, setMenu] = useState("");
+  const menuRef = useRef();
 
-    const [menu,setMenu] = useState("")
-    const menuRef = useRef();
-
-    const dropdown_toggle = (e) =>{
-       menuRef.current.classList.toggle('nav-menu-visible');
-       e.target.classList.toggle('open');
-    }
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle("nav-menu-visible");
+    e.target.classList.toggle("open");
+  };
   return (
-    <div className='navbar'>
-      <div className='nav-logo'>
-       <Link to='/'> <img src={mainlogo} alt=''/></Link>
+    <div className="navbar">
+      <div className="nav-logo">
+        <Link to="/">
+          {" "}
+          <img src={mainlogo} alt="" />
+        </Link>
       </div>
-      <img className='nav-dropdown' onClick={dropdown_toggle} src={navdropdown} alt=''/>
-      <ul ref={menuRef} className='nav-menu'>
-        <li onClick={()=>{setMenu("home")}}><Link style={{textDecoration: 'none'}} to='/'>Home</Link>{menu==="home"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration: 'none'}} to='/mens'>Mens</Link>{menu==="mens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration: 'none'}} to='/womens'>Womens</Link>{menu==="womens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration: 'none'}} to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+      <img
+        className="nav-dropdown"
+        onClick={dropdown_toggle}
+        src={navdropdown}
+        alt=""
+      />
+      <ul ref={menuRef} className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("home");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/">
+            Home
+          </Link>
+          {menu === "home" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("mens");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/mens">
+            Mens
+          </Link>
+          {menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("womens");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/womens">
+            Womens
+          </Link>
+          {menu === "womens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/kids">
+            Kids
+          </Link>
+          {menu === "kids" ? <hr /> : <></>}
+        </li>
       </ul>
       <div className="nav-login-cart">
-  
-  <Link to="/cart">
-    <img className="cart-icon" src={cart_icon} alt="" />
-  </Link>
-  <img className="loginicon" onClick={() => setShowLogin(true)} src={loginicon} alt="loginicon" />
-  <div className="nav-cart-count">0</div>
-</div>
-
+        <Link to="/cart">
+          <img className="cart-icon" src={cart_icon} alt="" />
+        </Link>
+        <img
+          className="loginicon"
+          onClick={() => setShowLogin(true)}
+          src={loginicon}
+          alt="loginicon"
+        />
+        <div className="nav-cart-count">0</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
